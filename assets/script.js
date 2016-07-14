7a120072740c871a3c2c23f99c91c2615611f5c8
Betty Chang <bwchang@berkeley.edu>
2016-07-14 12:53:49 -0400

    delete duplicates

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,18 @@
 $(document).ready(function(){
 
 	NUM_IMAGES = 103;
+	IMG_LOCATION = "assets/img/";
 	IMG_SUFFIX = ".jpg";
 	GIF_SUFFIX = ".GIF";
 
 	$("#refresh").click(function () {
 		var randomNumber = Math.floor(Math.random() * NUM_IMAGES) + 1;
 		if (randomNumber == 7 || randomNumber == 73 || randomNumber == 75) {
-			var imgName = randomNumber + GIF_SUFFIX;
+			var imgName = IMG_LOCATION + randomNumber + GIF_SUFFIX;
 		} else {
-			var imgName = randomNumber + IMG_SUFFIX;
+			var imgName = IMG_LOCATION + randomNumber + IMG_SUFFIX;
 		}
 		document.getElementById("imageid").src= imgName ;
 	});
-
+	
 });
