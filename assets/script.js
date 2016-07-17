968f56222fa61ed3b818623b95b41c800afb1dc4
Betty Chang <bwchang@berkeley.edu>
2016-07-17 11:26:30 -0400

    refresh text

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
 
-	NUM_IMAGES = 103;
+	NUM_IMAGES = 130;
 	IMG_LOCATION = "assets/img/";
 	IMG_SUFFIX = ".jpg";
 	GIF_SUFFIX = ".GIF";
 
 	$("#refresh").click(function () {
 		var randomNumber = Math.floor(Math.random() * NUM_IMAGES) + 1;
-		if (randomNumber == 7 || randomNumber == 73 || randomNumber == 75) {
+		if (randomNumber == 7 || randomNumber == 73 || randomNumber == 75 || randomNumber == 106 || randomNumber == 107) {
 			var imgName = IMG_LOCATION + randomNumber + GIF_SUFFIX;
 		} else {
 			var imgName = IMG_LOCATION + randomNumber + IMG_SUFFIX;
