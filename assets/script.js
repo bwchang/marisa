e0006e31cbbb5b0d81f8f3f4cfcc20ba8ff5dfae
Betty Chang <bwchang@berkeley.edu>
2016-07-17 12:11:36 -0400

    show pic logic

--- assets/script.js
+++ assets/script.js
@@ -5,9 +5,28 @@ $(document).ready(function(){
 	IMG_SUFFIX = ".jpg";
 	GIF_SUFFIX = ".GIF";
 
+	var shown = [];
+	var tries = 0;
+
+	var gifs = [7, 73, 75, 106, 107];
+
 	$("#refresh").click(function () {
 		var randomNumber = Math.floor(Math.random() * NUM_IMAGES) + 1;
-		if (randomNumber == 7 || randomNumber == 73 || randomNumber == 75 || randomNumber == 106 || randomNumber == 107) {
+		while (tries < 10) {
+			if (shown.length == 130) {
+				shown = [];
+				break;
+			}
+			if (shown.includes(randomNumber)) {
+				tries += 1;
+				randomNumber = Math.floor(Math.random() * NUM_IMAGES) + 1;
+			} else {
+				shown.push(randomNumber);
+				tries = 0;
+				break;
+			}
+		}
+		if (gifs.includes(randomNumber)) {
 			var imgName = IMG_LOCATION + randomNumber + GIF_SUFFIX;
 		} else {
 			var imgName = IMG_LOCATION + randomNumber + IMG_SUFFIX;
